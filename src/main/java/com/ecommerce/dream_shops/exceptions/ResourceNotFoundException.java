97bc4a3ed0941aecc1e5cc59270334064c2b2987
Daniel E. Zapata <daniel-dezz@hotmail.com>
2026-09-03 16:59:02 +0200

    feat: Enhance ImageController with download, update, and delete functionalities; add ResourceNotFoundException

---
+++ src/main/java/com/ecommerce/dream_shops/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.ecommerce.dream_shops.exceptions;
+
+/**
+ * ResourceNotFoundException
+ */
+public class ResourceNotFoundException extends RuntimeException {
+
+		public ResourceNotFoundException(String message) {
+				super(message);
+		}
+
+}
